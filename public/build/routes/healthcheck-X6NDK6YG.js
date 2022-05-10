99efcdbdb9969b711853c9ebfb01b650ff5d06b9
WTFICCTSN <jesus98.pt@gmail.com>
2022-05-10 17:47:50 +0100

    remix.run installed
    Co-Authored-By: toninho123 <39085060+toninho123@users.noreply.github.com>
    Co-Authored-By: ZephyrDogma <77338971+zephyrdogma@users.noreply.github.com>

---
+++ public/build/routes/healthcheck-X6NDK6YG.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:/home/wtficctsn/PhpstormProjects/TICKIT/app/routes/healthcheck.tsx?browser
+var require_healthcheck = __commonJS({
+  "browser-route-module:/home/wtficctsn/PhpstormProjects/TICKIT/app/routes/healthcheck.tsx?browser"(exports, module) {
+    init_react();
+    module.exports = {};
+  }
+});
+export default require_healthcheck();
+//# sourceMappingURL=/build/routes/healthcheck-X6NDK6YG.js.map
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require("@tailwindcss/aspect-ratio"),
+      require("@tailwindcss/forms"),
+  ],
+};
